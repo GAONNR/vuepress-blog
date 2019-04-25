38ba2303f8e841ae0be1d4d25be337be61023c84
Junseop Ji <gaon0403@gmail.com>
2019-04-25 15:55:50 +0900

    Remove deploy.sh (Now hosted by surge.sh)

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-vuepress build docs
-
-echo blog.g40n.xyz > docs/.vuepress/dist/CNAME
-
-git add docs/.vuepress/dist && git commit -m "Deploy"
-
-git subtree push --prefix docs/.vuepress/dist origin gh-pages
